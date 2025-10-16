7af0541402a556774a00926fcc9c92446a2f19bb
kheun-jav <kiheun00@gmail.com>
2025-10-16 13:52:00 +0900

    2025-10-16

--- plot_ly_study.R
+++ plot_ly_study.R
@@ -88,3 +88,64 @@ p4 |>
   add_markers(symbol = ~cyl)
 #기존 ggplot에서는 오류가 발생하는 case(연속형 변수를 shape에 매핑)
 #큰 효과를 보기는 어려워보임
+
+#사용자 지정 : I()안에 모양 번호 지정
+p41<- p4 |> 
+  add_markers(name = "default")
+p42<- p4 |> 
+  add_markers(symbol = I(1), name= "I(1)")
+subplot(p41, p42, margin = 0.05)
+
+#산점도 점 크기 : size
+#Animation 효과로 사용 가능
+p5<-plot_ly(mpg, x = ~displ, y = ~hwy)
+p51<- p5 |> 
+  add_markers(size = ~cyl, name = "default")
+p52<- p5 |> 
+  add_markers(size = ~cyl, sizes = c(50,100),
+              name = "custom")
+subplot(p51,p52, margin = 0.05)
+
+#사용자 지정 : I() 안에 필셀 단위로 크기 지정
+p5 |> 
+  add_markers(alpha = 0.3, size = I(20),
+              color = I("black"))
+
+#각 조합마다의 개수를 n 변수에 저장하고 중복제거
+mpg_1<-mpg |> 
+  group_by(displ,hwy) |> 
+  mutate(n = n()) |> 
+  distinct(displ, hwy, .keep_all = TRUE)
+mpg_1 |> 
+  select(manufacturer,model,displ,hwy,n)
+#데이터 개수를 점의 크기에 적용하여 그래프 작성
+#hover시 text에 해당 데이터의 개수를 표현
+plot_ly(mpg_1,x = ~displ, y = ~hwy) |> 
+  add_markers(size = ~n,
+              hoverinfo = "text",
+              text = paste(mpg_1$n, "data points"))
+
+#선그래프
+#add_lines()와 add_paths()활용
+#economics = 1967.07 ~ 2015.04 까지의 미국 경제 지표 시계열 자료
+data(economics, package="ggplot2")
+str(economics)
+
+economics |> 
+  plot_ly(x = ~date, y = ~unemploy) |> 
+  add_lines()
+#date의 순서로 입력되어 있음
+
+#2008 ~ 20213까지의 umemploy의 월별 시계열 그래프를
+#연도별로 구분해서 작성
+economics |> 
+  mutate(year = year(date), month = month(date)) |> 
+  filter(between(year, 2008, 2013)) |> 
+  plot_ly(x = ~ month, y = ~unemploy) |> 
+  add_lines(color = ~as.factor(year))
+#date 자료형의 자료를 year(), month()를 사용하여 년 / 월로 분리 가능
+#between()을 사용하여 2008 ~ 2013년도 자료만 filtering
+#plot_ly로 선그래프 작성 : color 옵션에 년도 넣을때 요인으로 변환
+
+#분석 : 2008년 실업자수 급증, 2009년도 증가하다 2010년 이후로는 감소하는 추세를 보인다
+#2008년 금융위기 사태가 원인으로 보임.
